df797eb00f35cce09c47e19ea4ee8e4aa23118c3
juan <jfazleal@icloud.com>
2022-08-17 12:48:12 -0700

    Tarea 1 - problemas 5, 9, 18 terminados, intentado problema 6

--- tarea-01/tarea-01/problemas.rkt
+++ tarea-01/tarea-01/problemas.rkt
@@ -36,8 +36,37 @@
 ;; 5.
 
 (define (filter x ls)
+  (cond
+    [(empty? ls) null]
+    [(x (first ls)) (cons (first ls) (filter x (rest ls)))]
+    [else  (filter x (rest ls))]))
+
+;; 6.
+
+#|(define (zip ls ls)
+  ...)|#
+
+;; 9.
+
+(define (reverse ls)
   (if (empty? ls)
       null
-      "no esta vacia"))
+      (append (reverse (rest ls))
+              (list (first ls)))))
+
+;; 18.
+
+(define (powerset ls)
+  (if (empty? ls)
+      (list ls)
+      (let ([ps (powerset (rest ls))])
+        (append (f (first ls) ps)
+        ps))))
+
+(define (f x ls)
+  (if (empty? ls)
+      null
+      (cons (cons x (first ls))
+            (f x (rest ls)))))
 
 (provide (all-defined-out))
