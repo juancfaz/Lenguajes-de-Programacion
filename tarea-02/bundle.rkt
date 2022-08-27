b3bbbb17b404414ec2d8d737af9d8f4a6142193b
Juan Faz <jfazleal@icloud.com>
2022-08-27 15:27:24 -0700

    Tarea 2 - bundle

---
+++ tarea-02/bundle.rkt
@@ -0,0 +1,36 @@
+#lang racket
+
+(define (unit-string-list? x)
+  (or (null? x)
+      (and (pair? x)
+           (string? (first x))
+           (= (string-length (first x)) 1) (unit-string-list? (rest x)))))
+
+(define (explode s)
+  (unless (string? s)
+    (error 'explode "esperaba una cadena, pero recibí: ~e" s))
+  (map string (string->list s)))
+
+(define (implode ls)
+  (unless (unit-string-list? ls)
+    (error 'implode "esperaba una lista de cadenas unitarias, pero recibí: ~e" ls))
+  (apply string-append ls))
+
+(define (take l n)
+  (cond
+    [(empty? l) null]
+    [(= n 0) null]
+    [else (cons (first l) (take (rest l) (- n 1)))]))
+
+(define (drop l n)
+  (cond
+    [(= n 0) l]
+    [(empty? l) null]
+    [else (drop (rest l) (- n 1))]))
+
+(define (bundle s n)
+  (cond
+    [(null? s) null]
+    [else
+     (cons (implode (take s n))
+           (bundle (drop s n) n))]))
