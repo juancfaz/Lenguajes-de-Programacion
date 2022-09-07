f845db5b5f7b7b7a7bbe281c3a61ac853ab4a3e0
Juan Faz <jfazleal@icloud.com>
2022-09-07 11:35:23 -0700

    Tarea 2 - problema 16

--- tarea-02/problemas.rkt
+++ tarea-02/problemas.rkt
@@ -1,77 +1,83 @@
 #lang racket
 
+; unit-string? : -> boolean
+(define (unit-string? x)
+  (and (string? x)
+       (= (string-length x) 1)))
+
+; unit-string-list? : list? -> boolean
 (define (unit-string-list? x)
   (or (null? x)
       (and (pair? x)
            (string? (first x))
-           (= (string-length (first x)) 1) (unit-string-list? (rest x)))))
+           (= (string-length (first x)) 1)
+           (unit-string-list? (rest x)))))
 
+; explode : string? -> list
 (define (explode s)
   (unless (string? s)
     (error 'explode "esperaba una cadena, pero recibí: ~e" s))
   (map string (string->list s)))
 
+; implode : list? -> string
 (define (implode ls)
   (unless (unit-string-list? ls)
     (error 'implode "esperaba una lista de cadenas unitarias, pero recibí: ~e" ls))
   (apply string-append ls))
 
+; take : list?, integer? -> list
 (define (take l n)
   (cond
     [(empty? l) null]
     [(= n 0) null]
     [else (cons (first l) (take (rest l) (- n 1)))]))
 
+; drop : list?, integer? -> list
 (define (drop l n)
   (cond
     [(= n 0) l]
     [(empty? l) null]
     [else (drop (rest l) (- n 1))]))
 
+; list->chunks : list?, integer? -> list
 (define (list->chunks ls n)
   (cond
     [(empty? ls) null]
     [else (cons (implode (take ls n)) (list->chunks (drop ls n) n))]))
 
+; bundle : (list-of-symbol?), integer? -> list
 (define (bundle s n)
   (cond
     [(null? s) null]
     [else (list->chunks s n)]))
 
+; partition : string?, integer? -> list
 (define (partition s n)
   (cond
     [(= (string-length s) 0) null]
     [else (cons (substring (mod s n) 0 n) (partition (substring (mod s n) n (string-length (mod s n)) ) n))]))
 
+; mod : string?, integer? -> string
 (define (mod s n)
   (cond
     [(not (zero? (modulo (string-length s) n))) (mod (string-append s " ") n)]
     [else s]))
 
-(define (empty ls)
-  (cond
-    [(empty? ls) null]
-    [else (cons (first ls) (empty (rest ls)))]))
-
-(define (remv-wh ls)
-  (cond
-    [(empty? ls) null]
-    [(not (eq? (rest ls) " ")) (cons (first ls) (remv-wh (rest ls)))]
-    [else null]))
-
+; isort : list?, proc? -> list
 (define (isort ls predicado)
   (if (empty? ls)
       null
       (insert (first ls)
               (isort (rest ls) predicado) predicado)))
  
-
-(define (insert n ls predicado)
+; insert : integer?, list?, proc? -> list
+(define (insert n ls arg)
   (cond
     [(empty? ls) (list n)]
-    [(predicado n (first ls)) (cons n ls)]
-    [else (cons (first ls) (insert n (rest ls) predicado))]))
+    [(arg n (first ls)) (cons n ls)]
+    [else (cons (first ls) (insert n (rest ls) arg))]))
 
+; quicksort : list?, proc? -> list
 (define (quicksort ls arg)
   (unless (procedure? arg) (error 'quicksort "Esperaba un predicado valido, recibi ~e" arg))
   (cond
